40a3b31fa384832b16c814b3695717a165df0236
Ami-Shahadat-Hossain <shahadat.user@gmail.con>
2023-07-18 21:36:26 +0600

    authentication_with_jwt

---
+++ README.md
@@ -0,0 +1,21 @@
+Generating Mailables Documetation: https://laravel.com/docs/10.x/mail#generating-mailables
+
+## Generate mailable class command
+
+```
+php artisan make:mail OTPMail
+
+```
+
+## SMTP Setup
+
+```
+MAIL_MAILER=smtp
+MAIL_HOST=mail.teamrabbil.com
+MAIL_PORT=587
+MAIL_USERNAME=info@teamrabbil.com
+MAIL_PASSWORD=~sR4[bhaC[Qs
+MAIL_ENCRYPTION=tls
+MAIL_FROM_ADDRESS="info@teamrabbil.com"
+MAIL_FROM_NAME="POS Project"
+```
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/users', 'allUsers');
+    Route::post('/register', 'registration');
+    Route::post('/user/login', 'userLogin');
+    Route::post('/send/otp', 'sendOtp');
+    Route::post('/verify/otp', 'verifyOtp');
+    Route::post('/reset/password', 'resetPassword')->middleware('verify.token');
+});
+
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard');
+});
+
+Route::get('/', function () {
+    return view('pages.frontend.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('pages.frontend.register');
+})->name('register');
+
+Route::get('/forgot/password', function () {
+    return view('pages.frontend.forgot_password');
+})->name('forgot.password');
+
+Route::get('/verify/otp', function () {
+    return view('pages.frontend.verify_otp');
+})->name('verify.otp');
+
+Route::get('/reset/password', function () {
+    return view('pages.frontend.reset_password');
+})->name('reset.password');
